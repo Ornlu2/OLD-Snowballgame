e247b5a4babd9ae2a2a5a065ec953d964886f4c9
Dylan Alter <alterdylan@gmail.com>
2017-11-29 20:25:04 -0500

    work in class

--- ballmountaingame/Assets/Scripts/IceShards.cs
+++ ballmountaingame/Assets/Scripts/IceShards.cs
@@ -7,29 +7,28 @@ public class IceShards : MonoBehaviour {
 
     public UnityEvent IceShard;
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
     void OnCollisionEnter2D(Collision2D col)
     {
+
         if (col.rigidbody.tag == "Player")
         {
 
-            var SnowballSize = col.gameObject.GetComponent<PlayerSnowBallController>();
-            SnowballSize.SnowBallSize -= SnowballSize.SnowAmountDecrease * 4;
+           // var SnowballSize = col.gameObject.GetComponent<PlayerSnowBallController>();
+            //SnowballSize.SnowBallSize -= SnowballSize.SnowAmountDecrease * 4;
             Debug.Log("ice hit player");
             IceShard.Invoke();
-
+            
 
         }
-        else
-        {
-            IceShard.Invoke();
-        }
+        
     }
 }
